3d744c6319d15ccaeab0e55f7f62705b655215e1
efbka <erik.fernandez@institutvidreres.cat>
2025-11-07 11:31:55 +0100

    comit3

--- .idea/RA3.JavaScript Variables.js
+++ .idea/RA3.JavaScript Variables.js
@@ -9,3 +9,20 @@
     console.log(numeros)
     console.log(letras)
     console.log(boolean)
+/*Exercici3*/
+    console.log(tardia)
+    var tardia = "llega tarde"
+    console.log(tardia)
+    /*console.log(nova)
+    let nova = "Falla"
+    console.log(nova)*/
+/*exercici4*/
+    var canv = "123"
+    console.log(canv)
+    canv = 123
+    console.log(canv)
+/*Exercici5*/
+    const cancon = 12
+    console.log(cancon)
+    cancon = "coco"
+    console.log(cancon)
