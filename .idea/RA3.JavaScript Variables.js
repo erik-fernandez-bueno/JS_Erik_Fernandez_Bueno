fe31a4d0b6d923d098e7d82410bd2f8a5bb6d44e
efbka <erik.fernandez@institutvidreres.cat>
2025-11-07 11:00:56 +0100

    Comit2

--- .idea/RA3.JavaScript Variables.js
+++ .idea/RA3.JavaScript Variables.js
@@ -1,3 +1,11 @@
+/* Exercici1*/
     console.log("Erik")
     console.log("Fernández Bueno")
     console.log("2004")
+/*Exercici2*/
+    var numeros = 2
+    let letras = "hola"
+    const boolean = true
+    console.log(numeros)
+    console.log(letras)
+    console.log(boolean)
