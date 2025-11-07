33059b552794e5439b61aa338b0186f3c44f7841
efbka <erik.fernandez@institutvidreres.cat>
2025-11-07 12:21:56 +0100

    comit3

---
+++ .idea/apartirdel6.js
@@ -0,0 +1,34 @@
+/*Exercici6*/
+    let num = 1234;        // number
+    let big = 5678n;        // BigInt
+    let bigAsNumber = Number(big);
+    let suma = num + bigAsNumber;
+    let resta = num - bigAsNumber;
+    let multiplicacion = num * bigAsNumber;
+    let division = num / bigAsNumber;
+    console.log("Suma:", suma);
+    console.log("Resta:", resta);
+    console.log("Multiplicación:", multiplicacion);
+    console.log("División:", division);
+/*Exercici7*/
+    let catet= 4
+    let hypotenusa= ((catet*catet) + (catet*catet))**0.5;
+    console.log(hypotenusa)
+/*Exercici8*/
+    let enter = 12
+    let string="24"
+    let dividir = enter / string
+    console.log(dividir)
+/*Exercici9*/
+    let nom= "Erik Fernández Bueno"
+    let cognom=(nom.slice(5,14))
+    console.log(cognom)
+    console.log(cognom.length)
+/*Exercici10*/
+    console.log(nom.slice(0,6),nom.slice(7))
+/*Exercici11*/
+    let a;
+    let b = null;
+    let suma = a + b;
+    console.log(suma);
+/*Exercici12*/
